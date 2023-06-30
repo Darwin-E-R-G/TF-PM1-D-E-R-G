aea6ec2629a3913cc882449110014dcbceb46d47
darwin <darwinrobles160@gmail.com>
2023-06-29 22:06:58 -0500

    se agrego el apartado del perfil

---
+++ proyecto/src/componentes/componeteperfil/perf.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function Perf() {
+  return (
+    <>
+      <h1> Perfil Profesional</h1>
+      <p>
+        Tecnólogo en Gestión de Redes y Sistema Teleinformáticos del ISER, se
+        capacitado de forma integral, humanística y técnica en las áreas de la
+        programación, las redes de comunicaciones y la adaptación de las nuevas
+        tecnologías en mi entorno, soportado en un modelo pedagógico que
+        incorpora un alto componente de integralidad del currículo para:
+        <br />
+        • Coordinar, Supervisar y promover el conjunto de actividades relacionadas
+          con el manejo de herramientas software, desarrollo de software e
+          implementación de Redes de Comunicación.
+        <br />
+        • Estructurar, y evaluar procesos tecnológicos para el desarrollo e
+          implementación de software.
+        <br />
+        • Estructurar, y evaluar procesos tecnológicos en la implementación de
+          Redes de Comunicaciones.
+        <br />
+        • Integrar interdisciplinariamente en la formulación y evaluación de
+          proyectos de Redes y Sistemas Teleinformáticos.
+        <br />
+        • Supervisar las operaciones de adaptación de nuevas tecnologías en
+          entornos organizacionales.
+        <br />
+        • Supervisar y coordinar las actividades de los trabajadores encargados de
+          las diversas operaciones en el campo de las redes y sistemas.
+        <br />
+        • Organiza establecimientos que proveen servicios técnicos en el campo de
+          las redes de comunicaciones, Sistema Teleinformáticos y Sistemas de
+          Información
+      </p>
+    </>
+  );
+}
+
+export default Perf;
--- proyecto/src/componentes/perfil.jsx
+++ proyecto/src/componentes/perfil.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Perf from "./componeteperfil/perf";
+import logo from "./imagenes/logo.png";
 
 function Perfil() {
   return (
-    <div className="q">
-      hola perfil
+    <div className="q expe" style={{height:"730px"}}>
+      <div className="wrapper">
+        <img src={logo} alt="Avatar2" className="avatar2" />
+        <Perf></Perf>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Perfil
+export default Perfil;
